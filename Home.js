c5bf9feadd8aab72fa7adcb76f663dc97059b0ca
jasbeer56419 <jasbeergurjar56419@gmail.com>
2025-05-21 17:09:28 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const Home = () => <h2>Welcome to Facebook Clone Home</h2>;
+export default Home;
---
+++ Signup.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const Signup = () => <h2>Signup Page</h2>;
+export default Signup;
